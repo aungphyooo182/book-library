4ff88fafa053c46ebae398365e7b1fa49d95e899
Aung Phyo Oo <aungphyooo@amdon.com>
2021-06-24 21:34:14 +0630

    text added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'pages-of-life';
+  title = "pages-of-life";
+  constructor(private router: Router) {}
+  admin() {
+    this.router.navigateByUrl("/admin");
+  }
 }
